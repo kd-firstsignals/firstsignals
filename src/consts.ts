f6f5dfa296412a2a92bb861db59db7827a8d56a7
Karishma Daga <karishdaga@gmail.com>
2025-10-16 20:28:39 -0400

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'FirstSignals - AI Credit Risk Management';
+export const SITE_DESCRIPTION = 'FirstSignals | AI for Commercial and Private Credit Risk Management';
+export const SITE_URL = 'https://firstsignals.io';
